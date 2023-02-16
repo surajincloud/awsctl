12468bb16e62ce0137f8b16491db121f5539bec0
Suraj Narwade <surajnarwade@thelife.local>
2023-02-16 10:53:50 +0000

    stream 1: initial commit

---
+++ cmd/s3.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// s3Cmd represents the s3 command
+var s3Cmd = &cobra.Command{
+	Use:   "s3",
+	Short: "Print s3 related information",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("s3 called")
+	},
+}
+
+func init() {
+	getCmd.AddCommand(s3Cmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// s3Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// s3Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
