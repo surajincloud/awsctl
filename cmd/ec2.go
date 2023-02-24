4ae969f310dfe5a68d8070a5fe827adf80fd2766
riteshsonawane1372 <riteshsonawane1372@gmail.com>
2023-02-24 06:43:28 +0530

    Added ec2 describe function

--- cmd/ec2.go
+++ cmd/ec2.go
@@ -4,9 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/spf13/cobra"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
 // ec2Cmd represents the ec2 command
@@ -15,10 +19,56 @@ var ec2Cmd = &cobra.Command{
 	Short: "Print ec2 related information",
 	Long: `For example,
 		awsctl get ec2`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ec2 called")
-	},
+	Run: ec2FunDescribe,
 }
+
+
+type EC2DesAPI interface {
+	DescribeInstances(ctx context.Context,
+		params *ec2.DescribeInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.DescribeInstancesOutput, error)
+}
+
+
+// Returns info about ec2 instance 
+
+func GetEc2Info(ctx context.Context,input *ec2.DescribeInstancesInput,api EC2DesAPI) (*ec2.DescribeInstancesOutput,error) {
+
+	return api.DescribeInstances(ctx,input)
+
+}
+
+func ec2FunDescribe (cmd * cobra.Command,args [] string){
+
+	ctx:= context.TODO() // Unclear Context
+	cfg,err:=config.LoadDefaultConfig(ctx)
+	if err!=nil{
+		log.Fatal("Config Error Occured",err)
+	}
+
+	client:= ec2.NewFromConfig(cfg)
+
+	input:=&ec2.DescribeInstancesInput{} // Res is DryRunOperation if everything is OK
+
+	info,err:=GetEc2Info(ctx,input,client)
+	if err!= nil{
+		log.Fatal("Error Occured while retrieving Information",err)
+	}
+
+	for _ , r:= range (info.Reservations){
+		fmt.Printf("\n Reservations ID : %s ",*r.ReservationId)
+		fmt.Printf("\n Reservations ID : %s ",*r.OwnerId)
+		fmt.Printf("\n Reservations ID : %s \n",*r.RequesterId)
+		
+		for _ ,i:= range r.Instances{
+
+			fmt.Printf("\n Instances ID: %s %s",i.LaunchTime,*i.InstanceId)
+		}
+	}
+
+
+
+} 
 
 func init() {
 	getCmd.AddCommand(ec2Cmd)
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.23 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.30 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.86.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.24 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.23 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.18.5 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
