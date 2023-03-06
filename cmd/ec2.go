62d2d9d9e11ab92dce5ab8451a3189f1ec43d382
adityasimant <adityasimant88@gmail.com>
2023-03-06 08:56:26 +0530

    commit

--- cmd/ec2.go
+++ cmd/ec2.go
@@ -7,36 +7,66 @@ import (
 	"fmt"
 	"os"
 	"text/tabwriter"
+
 	"github.com/spf13/cobra"
 	"github.com/surajincloud/awsctl/pkg"
 )
 
-// ec2Cmd represents the ec2 command
 var ec2Cmd = &cobra.Command{
 	Use:   "ec2",
 	Short: "Print ec2 related information",
 	Long: `For example,
 		awsctl get ec2`,
-	Run: getEC2command,
+	Run: func(cmd *cobra.Command, args []string) {
+		tag, _ := cmd.Flags().GetStringSlice("tags")
+		if len(tag) != 0 {
+			getEc2instanceTag(cmd, args)
+		} else {
+			getEC2command(cmd, args)
+		}
+
+	},
+}
+var keyPair = [2]string{"", ""}
+
+func getEc2instanceTag(cmd *cobra.Command, args []string) {
+	var ec2Instance []pkg.EC2Instance
+	tags, _ := cmd.Flags().GetStringSlice("tags")
+
+	ec2Instance = pkg.GetEC2InstanceTag(&tags[0], tags[1:])
+
+	w := tabwriter.NewWriter(os.Stdout, 18, 5, 3, ' ', tabwriter.TabIndent)
+	defer w.Flush()
+
+	fmt.Fprintln(w, "TagValue", "\t", "INSTANCE_ID", "\t", "INSTANCE_TYPE", "\t", "PRIVATE IP")
+	for _, i := range ec2Instance {
+		fmt.Fprintln(
+			w, i.InstanceName, "\t",
+			i.InstanceID, "\t",
+			i.InstanceType, "\t",
+			i.InstancePrivateIP)
+	}
 }
 
 func getEC2command(cmd *cobra.Command, args []string) {
 
-	var ec2Instance [] pkg.EC2Instance
-	ec2Instance =pkg.GetEC2Instance()
-	w:= tabwriter.NewWriter(os.Stdout,18,5,3,' ',tabwriter.TabIndent)
+	var ec2Instance []pkg.EC2Instance
+	ec2Instance = pkg.GetEC2Instance()
+	w := tabwriter.NewWriter(os.Stdout, 18, 5, 3, ' ', tabwriter.TabIndent)
 	defer w.Flush()
-	fmt.Fprintln(w,"NAME", "\t" ,"INSTANCE_ID","\t" ,"INSTANCE_TYPE", "\t", "PRIVATE IP")
-	for _,i:=range ec2Instance{
+
+	fmt.Fprintln(w, "NAME", "\t", "INSTANCE_ID", "\t", "INSTANCE_TYPE", "\t", "PRIVATE IP")
+	for _, i := range ec2Instance {
 		fmt.Fprintln(
-		w,i.InstanceName,"\t",
-		i.InstanceID,"\t",
-		i.InstanceType,"\t",
-		i.InstancePrivateIP,)
+			w, i.InstanceName, "\t",
+			i.InstanceID, "\t",
+			i.InstanceType, "\t",
+			i.InstancePrivateIP)
 	}
-	
+
 }
 
 func init() {
 	getCmd.AddCommand(ec2Cmd)
+	ec2Cmd.Flags().StringSlice("tags", []string{}, "get instance using key value Example: --tags=key,value1,value2")
 }
--- pkg/getEc2Info.go
+++ pkg/getEc2Info.go
@@ -9,50 +9,96 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
-
 type EC2Instance struct {
-
-	InstanceName 		string
-	InstanceID	 		string
-	InstanceType 		string
-	InstancePrivateIP 	string
-
+	InstanceName      string
+	InstanceID        string
+	InstanceType      string
+	InstancePrivateIP string
 }
 
-
-
-func GetEC2Instance()([]EC2Instance){
-	var ec2Instance [] EC2Instance
-
+func Ec2Client() (ec2.Client, context.Context) {
 	ctx := context.TODO()
-	cfg,err:=config.LoadDefaultConfig(ctx)
-
-	if err!=nil{
-		log.Fatal("Config Error Occured",err)
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal("Config Error Occured", err)
 	}
+	Ec2client := ec2.NewFromConfig(cfg)
+	return *Ec2client, ctx
+}
 
-	client := ec2.NewFromConfig(cfg)
-	
+func GetEC2Instance() []EC2Instance {
+
+	var ec2Instance []EC2Instance
+	client, ctx := Ec2Client()
 	input := &ec2.DescribeInstancesInput{}
-	info, err := client.DescribeInstances(ctx,input)
+	info, err := client.DescribeInstances(ctx, input)
 	if err != nil {
 		log.Fatal("Error Occured while retrieving Information", err)
 	}
-	
-	instanceName:="-"
+
+	instanceName := "-"
 	for _, r := range info.Reservations {
 		for _, i := range r.Instances {
-			for _,j:= range i.Tags{
-				if *j.Key == "Name"{
-					instanceName=aws.ToString(j.Value)
+			for _, j := range i.Tags {
+				if *j.Key == "Name" {
+					instanceName = aws.ToString(j.Value)
 				}
 			}
 			ec2Instance = append(ec2Instance, EC2Instance{
-				InstanceName: 		instanceName,
-				InstanceID:			aws.ToString(i.InstanceId) ,
-				InstanceType: 		*aws.String(string(*&i.InstanceType)),
-				InstancePrivateIP: 	aws.ToString(*&i.PrivateIpAddress),
+				InstanceName:      instanceName,
+				InstanceID:        aws.ToString(i.InstanceId),
+				InstanceType:      *aws.String(string(*&i.InstanceType)),
+				InstancePrivateIP: aws.ToString(*&i.PrivateIpAddress),
 			})
+		}
+	}
+
+	return ec2Instance
+}
+
+func GetEC2InstanceTag(tagName *string, tagValue []string) []EC2Instance {
+
+	input := &ec2.DescribeInstancesInput{}
+
+	client, _ := Ec2Client()
+
+	info, err := client.DescribeInstances(context.TODO(), input)
+	if err != nil {
+		log.Fatal("Error Occured while retrieving Information", err)
+	}
+
+	instanceName := ""
+	var ec2Instance []EC2Instance
+	for _, r := range info.Reservations {
+		for _, i := range r.Instances {
+			for _, j := range i.Tags {
+				if *j.Key == *tagName {
+					if len(tagValue) != 0 { // only if Values are passed
+						for _, value := range tagValue {
+							if *j.Value == *&value {
+								instanceName = aws.ToString(j.Value)
+								ec2Instance = append(ec2Instance, EC2Instance{
+									InstanceName:      instanceName,
+									InstanceID:        aws.ToString(i.InstanceId),
+									InstanceType:      *aws.String(string(*&i.InstanceType)),
+									InstancePrivateIP: aws.ToString(*&i.PrivateIpAddress),
+								})
+							}
+
+						}
+					} else { // if no Value is passed print all key
+						instanceName = aws.ToString(j.Value)
+						ec2Instance = append(ec2Instance, EC2Instance{
+							InstanceName:      instanceName,
+							InstanceID:        aws.ToString(i.InstanceId),
+							InstanceType:      *aws.String(string(*&i.InstanceType)),
+							InstancePrivateIP: aws.ToString(*&i.PrivateIpAddress),
+						})
+					}
+
+				}
+			}
+
 		}
 	}
 
