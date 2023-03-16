021aef346f6ee4525a4e8383982311907e21faef
Pratik gawande <gawandepratik022@.gmail.com>
2023-03-16 18:26:18 +0530

    created vpc.go and getVPC.go

---
+++ cmd/vpc.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"github.com/surajincloud/awsctl/pkg/VPC"
+)
+
+var vpcCmd = &cobra.Command{
+	Use:   "vpc",
+	Short: "Get VPCs",
+	Long:  "Get Vpcs with Name and Cidr",
+	RunE:  getVPC,
+}
+
+func getVPC(cmd *cobra.Command, args []string) error {
+
+	getVpc,err := VPC.GetVPC()
+	if err!=nil{
+		log.Fatal(err)
+	}
+	w := tabwriter.NewWriter(os.Stdout, 18, 5, 2, ' ', tabwriter.TabIndent)
+	defer w.Flush()
+
+	fmt.Fprintln(w,"NAME", "\t", "State", "\t", "DEFAULT", "\t", "IPv4CIDR","\t","VPCID")
+	for _,i:= range getVpc{
+		fmt.Fprintln(w,
+			*i.Name,"\t",
+			*i.State,"\t",
+			i.Default,"\t",
+			*i.Ipv4Cidr,"\t",
+			*i.VpcID)
+	}
+	return nil
+}
+
+func init() {
+	getCmd.AddCommand(vpcCmd)
+}
---
+++ pkg/VPC/getVPC.go
@@ -0,0 +1,53 @@
+package VPC
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+type Vpc struct {
+	Name     *string
+	State    *string
+	Ipv4Cidr *string
+	Default  bool
+	VpcID    *string
+}
+
+func GetVPC() ([]Vpc, error) {
+
+	var getVpc []Vpc
+
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := ec2.NewFromConfig(cfg)
+
+	info, err := client.DescribeVpcs(ctx, &ec2.DescribeVpcsInput{})
+	if err != nil {
+		return nil, err
+	}
+	for _, i := range info.Vpcs {
+		for _, j := range i.Tags {
+			if *j.Key == "Name" {
+				getVpc = append(getVpc, Vpc{
+					Name:     aws.String(*j.Value),
+					State:    aws.String(string(*&i.State)),
+					Ipv4Cidr: aws.String(*i.CidrBlock),
+					Default:  *i.IsDefault,
+					VpcID:    aws.String(*i.VpcId),
+				})
+			}
+
+		}
+	}
+
+	return getVpc, err
+
+}
