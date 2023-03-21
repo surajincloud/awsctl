a025918b03b5c1273f49dcecbfc44330bc282d6c
Ritesh Sonawane <72307121+riteshsonawane1372@users.noreply.github.com>
2023-03-21 17:40:13 +0530

    Update s3.go

--- cmd/s3.go
+++ cmd/s3.go
@@ -4,14 +4,16 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"text/tabwriter"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
-	"github.com/surajincloud/awsctl/pkg/s3API"
 )
 
 // s3Cmd represents the s3 command
@@ -24,23 +26,38 @@ var s3Cmd = &cobra.Command{
 }
 
 func getS3(cmd *cobra.Command, args []string) error {
-
-	w := tabwriter.NewWriter(os.Stdout, 10, 5, 2, ' ', tabwriter.TabIndent)
-	defer w.Flush()
-	fmt.Fprintln(w, "NAME", "\t", "CREATED_AT", "\t", "SIZE")
-
-	bucketList, err := s3API.GetBucket()
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		log.Fatal("Unable to get Data From Aws")
+		log.Fatal(err)
 	}
 
-	for _, i := range bucketList {
-		fmt.Fprintln(w, aws.String(i.Name), "\t", i.CreatedAt, "\t", i.Size, "KB")
-	}
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
 
+	out, err := client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := tabwriter.NewWriter(os.Stdout, 5, 2, 3, ' ', tabwriter.TabIndent)
+	defer w.Flush()
+	fmt.Fprintln(w, "NAME", "\t", "CREATED_AT")
+	for _, i := range out.Buckets {
+		fmt.Fprintln(w, aws.ToString(i.Name), "\t", i.CreationDate)
+	}
 	return nil
 }
 
 func init() {
 	getCmd.AddCommand(s3Cmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// s3Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// s3Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
