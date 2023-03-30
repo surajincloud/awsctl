70ed9f2215ea5f0232c0f58135cc9e079a3d302f
amalmadhu06 <amalmadhu06@gmail.com>
2023-03-30 14:07:59 +0530

    feat(auto completion): add auto completion
    added auto completion to improve user experience. User can use completion command to generate auto completion script for specific shells and run it

---
+++ cmd/completion.go
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023 NAME HERE <EMAIL ADDRESS>
+ */
+
+package cmd
+
+import (
+	"errors"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// completionCmd represents the completion command
+	completionCmd = &cobra.Command{
+		Use:     "completion [bash|zsh|powershell|fish]",
+		Short:   "Generates completion scripts for various shells",
+		Example: "awsctl completion [bash|zsh|powershell|fish]",
+		RunE:    completionCmdRun,
+	}
+
+	// completionBashCmd generates bash completion scripts
+	completionBashCmd = &cobra.Command{
+		Use:   "bash",
+		Short: "Generates bash completion scripts",
+		Run:   bashCompletion,
+	}
+
+	// completionFishCmd generates fish completion scripts
+	completionFishCmd = &cobra.Command{
+		Use:   "fish",
+		Short: "Generates fish completion scripts",
+		Run:   fishCompletion,
+	}
+
+	// completionPowerShellCmd generates PowerShell completion scripts
+	completionPowerShellCmd = &cobra.Command{
+		Use:   "powershell",
+		Short: "Generates PowerShell completion scripts",
+		Run:   powerShellCompletion,
+	}
+
+	// completionZshCmd generates Zsh completion scripts
+	completionZshCmd = &cobra.Command{
+		Use:   "zsh",
+		Short: "Generates Zsh completion scripts",
+		Run:   zshCompletion,
+	}
+)
+
+// completionCmdRun runs the completion command
+func completionCmdRun(cmd *cobra.Command, args []string) error {
+	err := cmd.Help()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return errors.New("a valid subcommand is required")
+}
+
+// bashCompletion generates bash completion scripts
+func bashCompletion(cmd *cobra.Command, args []string) {
+	err := rootCmd.GenBashCompletionFile("awsctl-completion.sh")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// fishCompletion generates fish completion scripts
+func fishCompletion(cmd *cobra.Command, args []string) {
+	err := rootCmd.GenFishCompletionFile("awsctl-completion.fish", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// powerShellCompletion generates PowerShell completion scripts
+func powerShellCompletion(cmd *cobra.Command, args []string) {
+	err := rootCmd.GenPowerShellCompletionFile("awsctl-completion.ps")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// zshCompletion generates Zsh completion scripts
+func zshCompletion(cmd *cobra.Command, args []string) {
+	err := rootCmd.GenZshCompletionFile("awsctl-completion.zsh")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	//adding subcommands to completion command
+	completionCmd.AddCommand(completionBashCmd)
+	completionCmd.AddCommand(completionZshCmd)
+	completionCmd.AddCommand(completionFishCmd)
+	completionCmd.AddCommand(completionPowerShellCmd)
+
+	//adding completion command to root
+	rootCmd.AddCommand(completionCmd)
+}
